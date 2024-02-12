81683971ced62f0b0f02aa74bb059c315710d61a
IsmaelDesu <143649074+IsmaelDesu@users.noreply.github.com>
2024-02-13 00:08:28 +0100

    Add files via upload

---
+++ video.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title></title>
+</head>
+
+<body>
+    <script src="//pull.cappuccicons.com/cpf.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,500,700,900|Hind:300,400,500|Poppins:200,300,400,500,600,700,900|Quicksand|Playfair+Display:400,700,700i,900,900i|Montserrat:400,500,600,700,900|Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+    <div id="ths-wallpaper1"></div>
+
+    <div class="listop-right1">
+        <div style="text-align: center; border-radius: 8px; position: relative;">
+            <video style="width: 9%; height: 5%; margin-left: -20px;">
+                <source src="Sassy.mp4" type="video/mp4">
+            </video>
+        </div>
+    </div>
+</body>
+
+</html>
